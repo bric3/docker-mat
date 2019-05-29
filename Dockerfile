bf3024ecbc6217546e5f7f50123f1d2925a3f3ac
Brice Dutheil <brice.dutheil@gmail.com>
2019-05-30 00:28:29 +0200

    Handle no arguments

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM adoptopenjdk/openjdk8:alpine-slim
 ARG MAT_VERSION=1.8.1
 ARG MAT_BUILD_DATE=20180910
 
-# RUN apk --no-cache add curl
 RUN wget -qO- "http://eclipse.mirror.garr.it/mirrors/eclipse//mat/${MAT_VERSION}/rcp/MemoryAnalyzer-${MAT_VERSION}.${MAT_BUILD_DATE}-linux.gtk.x86_64.zip" \
     | unzip - 
 RUN chmod +x /mat/MemoryAnalyzer /mat/ParseHeapDump.sh
@@ -13,5 +12,4 @@ VOLUME ["/dump"]
 
 ADD parse-dump.sh /
 
-# docker run -e JAVA_OPTS='-Xmx3g -Xms3g' --mount type=bind,source="$(pwd)",target=/dump dockermat dump.hprof
 ENTRYPOINT ["/parse-dump.sh"]
--- parse-dump.sh
+++ parse-dump.sh
@@ -5,7 +5,19 @@ heapdump=${1}
 reports="org.eclipse.mat.api:suspects org.eclipse.mat.api:overview org.eclipse.mat.api:top_components"
 # https://wiki.eclipse.org/MemoryAnalyzer/FAQ
 
+function usage() {
+    printf '%s\n' \
+        "Eclipse MAT in Docker" \
+        "Parses Java heap dumps in the command line. Reports will be generated" \
+        "in the same folder as the heap dump file." \
+        "" \
+        "Examples :"
+        "  docker run --mount type=bind,source=\"\$(pwd)\",target=/dump docker-mat heapdump.hprof" \
+        "  docker run -e JAVA_OPTS='-Xmx4g -Xms4g' --mount type=bind,source=\"\$(pwd)\",target=/dump docker-mat heapdump.hprof" \
+        ""
+}
 
+[[ -z "${heapdump}" ]] && { usage; exit 1; }
 [[ -f "${heapdump_volume}/${heapdump}" ]] && printf "Analyzing %s\n" ${heapdump} || { printf "'${heapdump}' does not exists\n"; exit 1; }
 
 
